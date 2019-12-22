151f2c3807c0bbd21d3318f39803976590f1e0b8
delaga <44586692+delaga@users.noreply.github.com>
2019-12-22 23:13:48 +0100

    4 zavrsio

--- PHP/polaznik30.edunova.hr/zimskiZadatak4.php
+++ PHP/polaznik30.edunova.hr/zimskiZadatak4.php
@@ -15,6 +15,47 @@
             <h3>Zadatak 04.</h3>
             <hr>
             <p>Kreirati program koji unosi dvije matrice 4x4 te ispisuje njihov zbroj</p>
+            <?php
+              $nizPrvi=[
+                [1,2,3,4],
+                [5,6,7,8],
+                [9,10,11,12],
+                [13,14,15,16]
+              ];
+              $nizDrugi=[
+                [17,18,19,20],
+                [21,22,23,14],
+                [25,26,27,28],
+                [29,30,31,32]
+              ];
+              echo '<h4>Prvi niz</h4>';
+              echo '<table>';
+              for ($i=0; $i < 4; $i++) { 
+                echo '<tr>';
+                for($j=0;$j<4;$j++){
+                    echo '<td>'.$nizPrvi[$i][$j].'</td>';
+                    $sum1+=$nizPrvi[$i][$j];
+                }
+                echo '</tr>';
+              }
+              echo '</table>';
+
+              echo '<h4>Drugi niz</h4>';
+              echo '<table>';
+              for ($i=0; $i < 4; $i++) { 
+                echo '<tr>';
+                for($j=0;$j<4;$j++){
+                    echo '<td>'.$nizDrugi[$i][$j].'</td>';
+                    $sum2+=$nizDrugi[$i][$j];
+                }
+                echo '</tr>';
+              }
+              echo '</table>';
+              $gotovo=$sum1+$sum2;
+              echo '<h4>Nihov zbroj je '.$gotovo.'</h4>';
+              
+              
+            ?>
           </div>
         </div>
       </div>
