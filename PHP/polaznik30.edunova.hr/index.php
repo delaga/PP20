157c9d4fcf2abcbee1f1149d75a96b8c6827d963
Andi Bašić <andi.basic10@gmail.com>
2020-01-20 18:57:17 +0100

    pon

--- PHP/polaznik30.edunova.hr/index.php
+++ PHP/polaznik30.edunova.hr/index.php
@@ -1,66 +1,23 @@
-<!doctype html>
-<html class="no-js" lang="en" dir="ltr">
-  <head>
+<?php
 
-    <?php include_once 'predlozak/head.php'; ?>
-    <?php //include_once 'predlozak/head1.php'; ?>
-    <?php //include 'predlozak/head.php'; ?>
-  </head>
-  <body>
-    <div class="grid-container">
-      <?php require_once 'predlozak/izbornik.php' ?>
-      <!-- Početak tijela -->
-      <div class="grid-x">
-        <div class="large-12 small-12 columns">
-          <div class="callout" style="min-height: 400px;"> 
-            Dobrodošli
-          </div>
-        </div>
-      </div>
-      <!-- Kraj tijela -->
-      <?php require_once 'predlozak/podnozje.php' ?>
-    </div>
-    <?php require_once 'predlozak/skripte.php' ?>
+define("BP", __DIR__ . DIRECTORY_SEPARATOR);
 
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Foundation for Sites</title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.css">
-  </head>
-  <body>
-    <div class="grid-container">
-      <!-- Početak izbornik -->
-            <div class="title-bar" data-responsive-toggle="example-animated-menu" data-hide-for="medium">
-        <button class="menu-icon" type="button" data-toggle></button>
-        <div class="title-bar-title">Menu</div>
-      </div>
+error_reporting(E_ALL);
+ini_set("display_errors",1);
 
-      <div class="top-bar" id="example-animated-menu" data-animate="hinge-in-from-top spin-out">
-        <div class="top-bar-left">
-          <ul class="dropdown menu" data-dropdown-menu>
-            <li class="menu-text">Site Title</li>
-            <li>
-              <a href="#">One</a>
-              <ul class="menu vertical">
-                <li><a href="#">One</a></li>
-                <li><a href="#">Two</a></li>
-                <li><a href="#">Three</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Two</a></li>
-            <li><a href="#">Three</a></li>
-          </ul>
-        </div>
-      </div>
-      <!-- Kraj zbornik -->
-    </div>
+$t = implode(PATH_SEPARATOR,
+        [
+            BP . "model",
+            BP . "controller"
+        ]
+    );
 
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/what-input.js"></script>
-    <script src="js/vendor/foundation.js"></script>
-    <script src="js/app.js"></script>
+    set_include_path($t);
 
-  </body>
-</html>
+    spl_autoload_register(function($klasa) 
+    {
+        $putanja = strtr($klasa,"\\",DIRECTORY_SEPARATOR) . ".php";
+        return include $putanja;
+    });
+
+    Test::izvediTest();
--- PHP/polaznik30.edunova.hr/predlozak/podnozje.php
+++
@@ -1,9 +0,0 @@
- <!-- Početak podnožja -->
- <div class="grid-x">
-    <div class="large-12 small-12 columns">
-    <div class="<?php echo $dev ? 'alert' : 'warning'; ?> callout"> 
-    &copy; <?php echo $nazivAPP; ?> 2019
-    </div>
-</div>
-</div>
-<!-- Kraj podnožja -->
